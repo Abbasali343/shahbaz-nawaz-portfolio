a0137c11bc1a16075210e73d946bbbae3698d5d2
Abbasali343 <60543462+Abbasali343@users.noreply.github.com>
2023-12-28 23:30:28 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+function onDownloadResumeHandler(value){
+  if(value==="1")
+  {
+    window.open("docs/Resume-Shahbaz.pdf")
+  }
+  else if(value==="2")
+  {
+    window.open("docs/Resume-Shahbaz.pdf")
+  }
+  document.getElementById("select-download-resume").value=0;
+  
+}
